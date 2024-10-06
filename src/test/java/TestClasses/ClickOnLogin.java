53dfb7c36f83e4a76472d663f74da1984e92e11a
Hanumant <hanuamntc0795@gmail.com>
2024-10-06 13:14:29 +0530

    m12

--- src/test/java/TestClasses/ClickOnLogin.java
+++ src/test/java/TestClasses/ClickOnLogin.java
@@ -13,8 +13,7 @@ import pom.SummitRegisterNow;
 public class ClickOnLogin extends BaseClass {
 
 	//SignIn
-//added new line
-
+    //added new line
 
 	Products_BillPay billPay;
 	SummitRegisterNow summit;
